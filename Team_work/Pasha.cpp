4f5fad87c085d7ccda15c01b90d6e7c59dc2256a
piblik <pashamatvijyk82@gmail.com>
2025-02-11 17:28:12 +0200

    Update Pasha.cpp
    Написав код для сортування авто за витратою палива, і для сортування по кілометражу який можуть проїхати авто без дозаправки. Підписав функції для зручності. :D

--- Team_work/Pasha.cpp
+++ Team_work/Pasha.cpp
@@ -1,7 +1,41 @@
-#include <iostream>
+// sort.h
+#ifndef SORT_H
+#define SORT_H
 
-using namespace std;
+#include "core.h"
 
-int main() {
-	cout << "Hello Jesus :D\nlalalallala\n";
+void sortCars(Car cars[], int n);
+void findMaxDistanceCars(Car cars[], int n);
+
+#endif // SORT_H
+
+// sort.cpp
+#include "sort.h"
+
+// Sort by fuel consumption
+void sortCars(Car cars[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (cars[i].fuelUsage > cars[j].fuelUsage) {
+                swap(cars[i], cars[j]);
+            }
+        }
+    }
+}
+
+// Search for cars that can travel the most without refueling
+void findMaxDistanceCars(Car cars[], int n) {
+    double maxDistance = 0;
+    for (int i = 0; i < n; i++) {
+        double distance = cars[i].tankVolume / (cars[i].fuelUsage / 100);
+        if (distance > maxDistance) {
+            maxDistance = distance;
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        double distance = cars[i].tankVolume / (cars[i].fuelUsage / 100);
+        if (distance == maxDistance) {
+            cout << cars[i].brand << " " << cars[i].type << " (" << distance << " κμ)" << endl;
+        }
+    }
 }
