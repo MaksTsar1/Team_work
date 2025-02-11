f14009b4c1ffdd22f190a841b55609fdb6c10335
JESUS <58883979+66shadow66@users.noreply.github.com>
2025-02-11 21:31:26 +0200

    ююююю

--- Team_work/Dima.cpp
+++ Team_work/Dima.cpp
@@ -1,7 +1,31 @@
-#include <iostream>
+﻿#ifndef ANALYSIS_H
+#define ANALYSIS_H
 
-using namespace std;
+#include "core.h"
 
-in main() {
-	cout << "Hello maks";
+void findMostAndLeastPowerful(Car cars[], int n);
+
+#endif // ANALYSIS_H
+
+// analysis.cpp
+#include "analysis.h"
+
+// Пошук найпотужнішого легкового і найменш потужного вантажного авто
+void findMostAndLeastPowerful(Car cars[], int n) {
+    int maxcP = 0, mintP = 10000;
+    string maxcB, mintB;
+
+    for (int i = 0; i < n; i++) {
+        if (string(cars[i].type) == "легковий" && cars[i].power > maxCarPower) {
+            maxcP = cars[i].power;
+            maxcB = cars[i].brand;
+        }
+        if (string(cars[i].type) == "вантажний" && cars[i].power < minTruckPower) {
+            mintP = cars[i].power;
+            mintP = cars[i].brand;
+        }
+    }
+
+    cout << "Найпотужніший легковий: " << maxcB << " (" << maxcP << " к.с.)\n";
+    cout << "Найменш потужний вантажний: " << mintB << " (" << mintP << " к.с.)\n";
 }
